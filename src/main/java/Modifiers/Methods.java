fc945eba78f9355845cf1b348e1c24c6e34115d0
ext.aklimenko <Fhxbr1996>
2020-02-04 20:59:31 +0200

    add a new tasks (OOP)

---
+++ src/main/java/Modifiers/Methods.java
@@ -0,0 +1,21 @@
+package Modifiers;
+
+public class Methods {
+    public void say(String str) {
+        System.out.println("You say: " + str);
+    }
+
+    private void run() {
+        System.out.println("I'm running!");
+    }
+
+    protected void age(int a) {
+        System.out.println("I am " + a + "years old");
+    }
+    void high (int h) {
+        System.out.println("my high is " + h);
+    }
+    static void arara (String say){
+        System.out.println(say);
+    }
+}

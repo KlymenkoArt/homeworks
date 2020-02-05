69bf4cc2ca65444f30e853bcb1fe15bf49b65bfa
ext.aklimenko <Fhxbr1996>
2020-02-05 22:00:50 +0200

    add a new tasks (Threads, Constructors)

---
+++ src/main/java/Constructors/Run.java
@@ -0,0 +1,9 @@
+package Constructors;
+
+public class Run {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Dog dog1 = new Dog("Sharik", 5);
+        Dog dog2 = new Dog("Tuzik", 4, 0.34);
+    }
+}
